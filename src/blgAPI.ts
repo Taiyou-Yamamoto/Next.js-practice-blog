8dcdceb68bc758096f751c85a384850fb67f04ef
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-10-19 10:16:15 +0900

    ローディング画面追加

--- src/blgAPI.ts
+++ src/blgAPI.ts
@@ -9,6 +9,8 @@ export const getAllArticle = async (): Promise<Article[]> => {
     throw new Error('エラーが発生しました');
   }
 
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
   const articles = await res.json();
   return articles;
 };

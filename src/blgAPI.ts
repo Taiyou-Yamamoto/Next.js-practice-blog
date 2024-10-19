3f959a91e4810b1f3fd5ca205ec31ebe33d2210f
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-10-19 10:05:00 +0900

    フェッチ

--- src/blgAPI.ts
+++ src/blgAPI.ts
@@ -1,7 +1,14 @@
 import { Article } from './type';
 
 export const getAllArticle = async (): Promise<Article[]> => {
-  const res = await fetch('https://localhost:3001/posts', {
+  const res = await fetch('http://localhost:3001/post', {
     cache: 'no-store',
-  });
+  }); //SSR
+
+  if (!res.ok) {
+    throw new Error('エラーが発生しました');
+  }
+
+  const articles = await res.json();
+  return articles;
 };

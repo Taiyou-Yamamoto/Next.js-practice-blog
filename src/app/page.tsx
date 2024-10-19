c6dc591e1a0dc26c77625f3aa613852631ed075c
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-10-19 10:05:27 +0900

    ページ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,29 @@
+// 'use client';
+
 import Image from 'next/image';
 import ArticleList from './components/ArticleList';
+import { getAllArticle } from '@/blgAPI';
+import { useEffect } from 'react';
 
-export default function Home() {
+export default async function Home() {
+  const articles = await getAllArticle();
+
+  // useEffect(() => {
+  //   const getAllBlogs = async () => {
+  //     const res = await fetch('http://localhost:3001/post');
+  //     const articles = await res.json();
+  //     console.log(articles);
+  //   };
+  //   getAllBlogs();
+  // }, []);
   return (
     <div className='md:flex'>
       <section className='w-full md:w-2/3 flex flex-col items-center px-3'>
-        <ArticleList />
+        <ArticleList articles={articles} />
       </section>
 
       <aside className='w-full md:w-1/3 flex flex-col items-center px-3 md:pl-6'>
+        x
         <div className='bg-white shadow-md rounded p-4 mb-6 mt-4'>
           <h3 className='font-bold text-gray-900 mb-2'>About Me</h3>
           <p className='text-gray-600'>

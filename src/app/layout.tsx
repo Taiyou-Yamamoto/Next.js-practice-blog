2387ee6223e45992e4199debd824d105d79e9ae1
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-10-19 10:16:37 +0900

    ローディングのためにSuspence追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import localFont from 'next/font/local';
 import './globals.css';
 import Header from './Header';
 import Footer from './Footer';
+import { Suspense } from 'react';
+import Loading from './loading';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -29,7 +31,7 @@ export default function RootLayout({
     <html lang='ja'>
       <body className='container mx-auto bg-slate-700 text-slate-50'>
         <Header />
-        {children}
+        <Suspense fallback={<Loading />}>{children}</Suspense>
         <Footer />
       </body>
     </html>

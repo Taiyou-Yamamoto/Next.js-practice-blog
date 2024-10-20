b7871b37e2b7bde5d5d6714354743afb2efc8f38
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-10-20 10:14:31 +0900

    詳細ページ

---
+++ src/app/articles/[id]/page.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import React from 'react';
+
+const Article = ({ params }: { params: { id: string } }) => {
+  //   console.log(params);
+  return (
+    <div className='max-w-3xl mx-auto p-5'>
+      <Image
+        src='https://images.unsplash.com/photo-1728567409684-e42ba81a3c34?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        alt=''
+        width={1280}
+        height={300}
+      />
+      <h1 className='text-4xl text-center mb-10 mt-10'>ここがたいとる</h1>
+      <div className='text-lg leading-relaxed text-justify'>
+        <p>ここが本文です</p>
+      </div>
+    </div>
+  );
+};
+
+export default Article;
